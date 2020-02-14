7649200fc0161f5d48ce1df43e133df20de58f8e
Paulo Bruno <pbrunosousa@gmail.com>
2020-02-14 10:48:49 -0300

    Check draw
    Problem
     - The game was only stopping if a win condition was met. If the game
     should end in a draw, it did not stop.
    
    Solution
     - Since each player board keeps a '1' only in positions in which there
     is one of your 'pieces', checking if the board (a draw) is only a
     matter of checking if the bitwise OR of both boards is full of '1'. The
     int value of full 9-bit binary value is 511. Thus, checking the draw
     means to check if (player 1 board OR player 2 board) === 511.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@
 2 5 8 = 001001001 = 73
 0 4 8 = 100010001 = 273
 2 4 6 = 001010100 = 84
+empate = 111111111 = 511
 
 # lógica para testar os valores que vencem
 const ref = '';
@@ -26,7 +27,9 @@ ref[position] = '1';
 
 */
 
-(function() {
+(function() {   
+    const DRAW_VALUE = 511
+
     let player1 = true;
     let stop = false;
     const victoryNumbers = [448, 56, 7, 292, 146, 73, 273, 84];
@@ -46,18 +49,33 @@ ref[position] = '1';
 
         celula.innerHTML = player1 ? "☕" : "☘️";
 
-        checkWinner(currentPositions, celula.innerHTML);
+        checkResult(positions1, positions2, celula.innerHTML);
 
         player1 = !player1;
     }
 
-    function checkWinner(playerPositions, currentPlayer) {
+    function checkResult(positions1, positions2, currentPlayer) {
+        let playerPositions = player1 ? positions1 : positions2;
         const refValue = parseInt(playerPositions.join(''), 2);
+
         const hasWinner = victoryNumbers.some((value) => (value & refValue) === value);
 
         if (hasWinner) {
             winnerMessage.innerHTML = currentPlayer + ' venceu!';
             stop = true;
+        }
+        else
+        {
+            const binPositions1 = parseInt(positions1.join(''), 2);
+            const binPositions2 = parseInt(positions2.join(''), 2);
+
+            const isDraw = ((binPositions1 | binPositions2) === DRAW_VALUE);
+
+            if (isDraw)
+            {
+                winnerMessage.innerHTML = ' Empate!';
+                stop = true;
+            }
         }
     }
 

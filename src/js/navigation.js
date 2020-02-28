5d3eacb975f9b411b840a6e38632435b45f8708a
Paulo Bruno <paulo.bruno@hp.com>
2020-02-28 14:53:05 -0300

    Hide table in menu. Fix #4

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -7,10 +7,12 @@ export default function createNavigation() {
         for (let page of PAGES) {
             const style = document.getElementById(page).style;
             if (page === url) {
+                style.display = 'block';
                 style.opacity = '1';
                 style.height = 'fit-content';
                 style.zIndex = '100';
             } else {
+                style.display = 'none';
                 style.opacity = '0';
                 style.height = '0px';
                 style.zIndex = '0';
